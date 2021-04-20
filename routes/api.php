c7273fdab0de671a13d8722ae6bc0a00ea99c086
blackcode@DESKTOP-CC0RJLG <aqilblackcode@gmail.com>
2021-04-20 14:28:37 +0700

    Praktikum 1 : Add traits apiResponse , add login and register requets and controller auth and fill 3 function [login , register , logout] and add to route api/*

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum'])->group(function(){
+	Route::post('/logout',[AuthController::class,'logout']);
+});
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
